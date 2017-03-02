fe9d6a42b83f015b454c8533f3524811df61571a
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-03 00:58:51 +0100

    maguro: Welcome LineageOS
    Change-Id: Ib8e9957282c8b005b9b6c0807e58c31202777aa2

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := GN-GSM
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Enhanced NFC
@@ -16,7 +16,7 @@ $(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
-PRODUCT_NAME := cm_maguro
+PRODUCT_NAME := lineage_maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
